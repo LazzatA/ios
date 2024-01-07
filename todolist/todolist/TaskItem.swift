c9c873a3e76e993384f5a3f322dd6bb10b549586
Lazzat Anafiya <97632745+LazzatA@users.noreply.github.com>
2024-01-07 14:09:11 -0800

    Add files via upload

---
+++ todolist/todolist/TaskItem.swift
@@ -0,0 +1,13 @@
+//
+//  TaskItem.swift
+//  todolist
+//
+//  Created by okokok on 07.01.2024.
+//
+
+import Foundation
+
+struct TaskItem: Codable {
+    var name = " "
+    var isCompleted = false
+}
